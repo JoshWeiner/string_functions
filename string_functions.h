aae5d5374582d1338d524ae3da0778f858fd02c5
aidris823 <anandidris@Anands-MacBook-Pro.local>
2018-10-01 01:26:44 -0400

    Added .h file

---
+++ string_functions.h
@@ -0,0 +1,6 @@
+int string_len(char string[]);
+char * string_copy(char* dest, char *source);
+char * string_cat(char* dest, char *source);
+int * string_cmp(char *s1, char *s2);
+char * string_chr(char *s, char c);
+
