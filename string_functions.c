5b159f45f21b8ce4e1c4fdb25e57a5a59d886ddb
aidris823 <anandidris@Anands-MacBook-Pro.local>
2018-10-01 01:40:08 -0400

    1.Forgot that one of them has to be the -n version, misread the initial problem as an array

--- string_functions.c
+++ string_functions.c
@@ -3,18 +3,23 @@
 #include <string.h>
 #include "string_functions.h"
 
-int string_len(char string[]){
-  int i = 0;
+int string_len(char* string){
   int ans = 0;
-  while (string[i] != 0){
-    i++;
+  while (*string){
+    string++;
     ans++;
   }
   return ans;
 }
 
+char * stringn_copy(char* dest, char*source){
+  return 0;
+}
+  
 int main(){
+  printf(" \n S1: [Talos guide you.] \n S2: [PSI Rockin OMEGA] \n S3: [Galileo & Figaro] \n");
   //System's functions:
+  printf("\n Testing string_len(s1) function: \n");
   char charizard[] = "Talos guide you.";
   int length = strlen(charizard);
   printf("System's length of |%s| : %d \n",charizard,length);
--- string_functions.h
+++ string_functions.h
@@ -1,5 +1,5 @@
 int string_len(char string[]);
-char * string_copy(char* dest, char *source);
+char * stringn_copy(char* dest, char *source);
 char * string_cat(char* dest, char *source);
 int * string_cmp(char *s1, char *s2);
 char * string_chr(char *s, char c);
