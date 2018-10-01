ae4bc77ed26950da4823345d34bf61c9db107793
aidris823 <anandidris@Anands-MacBook-Pro.local>
2018-10-01 01:22:14 -0400

    mrow

---
+++ string_functions.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+//#include "string_functions.h"
+
+int string_len(char string[]){
+  int i = 0;
+  int ans = 0;
+  while (string[i] != 0){
+    ans++;
+  }
+  return ans;
+}
+
+int main(){
+  //System's functions:
+  char charizard[] = "Talos guide you.";
+  int length = strlen(charizard);
+  printf("System's length of |%s| : %d \n",charizard,length);
+  int my_length = string_len(charizard);
+  printf("My length of |%s| : %d \n",charizard,length);
+}
