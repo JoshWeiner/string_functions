55f2c96d5d0cd4d1637ca7e1d858cbee9409ab0e
JoshWeiner <joshuaweinernyc@gmail.com>
2018-10-01 02:00:50 -0400

    Update string_functions.c

--- string_functions.c
+++ string_functions.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include "string_functions.h"
 
-int string_len(char* string){
+int string_len(char* str){
   int ans = 0;
-  while (*string){
-    string++;
+  while (*str){
+    str++;
     ans++;
   }
   return ans;
@@ -15,14 +15,26 @@ int string_len(char* string){
 char * stringn_copy(char* dest, char*source){
   return 0;
 }
+
+int string_comp( char* str1, char* str2 ){
+  while(*str1 && *str2){
+    if(*str1 != *str2){
+      return (*str1 - *str2);
+    }
+    str1++;
+    str2++;
+  }
+  return 0;
+}
   
 int main(){
-  printf(" \n S1: [Talos guide you.] \n S2: [PSI Rockin OMEGA] \n S3: [Galileo & Figaro] \n");
+  //printf(" \n S1: [Talos guide you.] \n S2: [PSI Rockin OMEGA] \n S3: [Galileo & Figaro] \n");
   //System's functions:
+  char s1[] = "charizard";
+  char s2[] = "pikachu";
+  char s3[] = "squirtle";
+  
   printf("\n Testing string_len(s1) function: \n");
-  char charizard[] = "Talos guide you.";
-  int length = strlen(charizard);
-  printf("System's length of |%s| : %d \n",charizard,length);
-  int my_length = string_len(charizard);
-  printf("My length of |%s| : %d \n",charizard,length);
+  printf("System's length of |%s| : %d \n",s1, strlen(s1));
+  printf("My length of |%s| : %d \n",charizard, string_len(charizard));
 }
